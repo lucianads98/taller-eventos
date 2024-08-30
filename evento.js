ab4fb6e9e66316401309dcdde922c7b5d152a178
Luciana De Souza <129917797+lucianads98@users.noreply.github.com>
2024-08-30 17:28:37 -0300

    Solucion click

--- evento.js
+++ evento.js
@@ -1,7 +1,12 @@
-function saludar() {
+
+function saludar(event) {
+    event.stopPropagation(); 
     alert('¡Hola!');
 }
 
 document.getElementById('contenedor-boton').addEventListener('click', function() {
     alert('¡Hola! Soy el div');
 });
+
+
+document.getElementById('miBoton').addEventListener('click', saludar);
